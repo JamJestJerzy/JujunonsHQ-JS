f1796fd389416dc7a40d6a8c63f1a5d55d6a4a5a
Jerzy <contact@j3rzy.dev>
2023-12-04 19:41:42 +0100

    Added attachments logging to logger

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -19,8 +19,41 @@ module.exports = {
 			.setTimestamp()
 			.setColor('#a3def8')
 
+		if (message.attachments.size > 0) {
+			let attachments = "";
+
+			message.attachments.forEach((attachment) => {
+				attachments = attachments + `[${attachment.name}](${attachment.url}) (${getSize(attachment.size)})\n`;
+			});
+
+			emb.addFields({
+				inline: false,
+				name: "Attachments",
+				value: attachments
+			})
+		}
+
 		let logChannel = await message.guild.channels.cache.find(r => r.name === 'message-logs')
 
 		return await logChannel.send({ embeds: [emb] })
 	},
 };
+
+function getSize(bytes) {
+	const kilobytes = bytes / 1024.0;
+	const megabytes = bytes / (1024.0 * 1024);
+	const gigabytes = bytes / (1024.0 * 1024 * 1024);
+	const terabytes = bytes / (1024.0 * 1024 * 1024 * 1024);
+
+	if (terabytes > 1) {
+		return `${terabytes.toFixed(2)} TB`;
+	} else if (gigabytes > 1) {
+		return `${gigabytes.toFixed(2)} GB`;
+	} else if (megabytes > 1) {
+		return `${megabytes.toFixed(2)} MB`;
+	} else if (kilobytes > 1) {
+		return `${kilobytes.toFixed(2)} KB`;
+	} else {
+		return `${bytes} bytes`;
+	}
+}
